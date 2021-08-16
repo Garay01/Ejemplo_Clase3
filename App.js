7ac992204ef950e74afc1cc98349ae72fe89323a
@Garay01 <garayalvarado14@gmail.com>
2021-08-16 11:29:24 -0600

    Crea primer proyecto con react native

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{
+          uri: "https://cdn.pixabay.com/photo/2018/09/19/23/03/sunset-3689760__340.jpg",
+        }}
+        style={{ width: 200, height: 100 }}
+      />
+      <Text style={[styles.text, styles.coral]}>
+        Bryan Walberto Garay Alvarado
+      </Text>
+      <Text style={styles.text}>GA181935</Text>
+      <Text style={styles.parr}>Ingeniería en Ciencias de la Computación</Text>
+      <Text style={styles.parr}>Lenguaje favorito: Javascript</Text>
+      <Text style={styles.parr}>Hobbies: Speedcube, fútbol</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: "1.5em",
+  },
+  parr: {
+    fontWeight: "bold",
+    fontSize: "1em",
+  },
+  coral: {
+    color: "coral",
   },
 });
